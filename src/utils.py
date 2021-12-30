72e001923b1d5b405ea10716aa97b6777445aeef
eqdatascience <55494041+eqdatascience@users.noreply.github.com>
2021-12-30 15:42:05 +0200

    modified prefilter_items

--- src/utils.py
+++ src/utils.py
@@ -1,64 +1,55 @@
-# Общая функция группировки, на потом - заменить дублирующийся код
-def f_popularity(data_train):
-    popularity = data_train.groupby('item_id')['quantity'].sum().reset_index()
-    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
-    return popularity
+import pandas as pd
+import numpy as np
 
-# Оставим только 5000 самых популярных товаров
-def prefilter_items(data_train):
-    popularity = data_train.groupby('item_id')['quantity'].sum().reset_index()
-    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
-    top_5000 = popularity.sort_values('n_sold', ascending=False).head(5000).item_id.tolist()
-    #добавим, чтобы не потерять юзеров
-    data_train.loc[~data_train['item_id'].isin(top_5000), 'item_id'] = 999999
-    return data_train
-      
-    
-# Уберем самые популярные
-def prefilter_items_most_popular(data_train,N=50):
-    popularity = data_train.groupby('item_id')['quantity'].sum().reset_index()
-    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
-    most_pop = popularity.sort_values('n_sold', ascending=False).head(N).item_id.tolist()
-    data_train.loc[data_train['item_id'].isin(most_pop), 'item_id'] = 999999
-    return data_train
-    
-    
-# Уберем самые непопулряные 
-def prefilter_items_not_popular(data_train,N=50):
-    popularity = data_train.groupby('item_id')['quantity'].sum().reset_index()
-    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
-    less_pop = popularity[popularity.n_sold<N].item_id.tolist()
-    data_train.loc[data_train['item_id'].isin(less_pop), 'item_id'] = 999999
-    return data_train
-      
-    
-# Уберем товары, которые не продавались за последние 12 месяцев
-def prefilter_items_12months(data_train):
-    data_train_12 = data_train[(data_train['week_no'].between(data_train['week_no'].max()-52,data_train['week_no'].max(), inclusive=True))].copy()
-    popularity = data_train_12.groupby('item_id')['quantity'].sum().reset_index()
-    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
-    no_12months = popularity[popularity.n_sold==0].item_id.tolist()
-    data_train.loc[data_train['item_id'].isin(no_12months), 'item_id'] = 999999
-    return data_train
+
+def prefilter_items(data, take_n_popular=5000, item_features=None):
+    # Уберем самые популярные товары (их и так купят)
+    popularity = data.groupby('item_id')['user_id'].nunique().reset_index() / data['user_id'].nunique()
+    popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
+
+    top_popular = popularity[popularity['share_unique_users'] > 0.8].item_id.tolist()
+    data = data[~data['item_id'].isin(top_popular)]
+
+    # Уберем самые НЕ популярные товары (их и так НЕ купят)
+    top_notpopular = popularity[popularity['share_unique_users'] < 0.01].item_id.tolist()
+    data = data[~data['item_id'].isin(top_notpopular)]
+
+    # Уберем товары, которые не продавались за последние 12 месяцев
+	data = data.loc[~(data['week_no'] < data['week_no'].max() - 52)]
 
     # Уберем не интересные для рекоммендаций категории (department)
-    
-    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб. 
-    
-    # Уберем слишком дорогие товары
-    
+    #if item_features is not None:
+    #    department_size = pd.DataFrame(item_features.\
+    #                                    groupby('department')['item_id'].nunique().\
+    #                                    sort_values(ascending=False)).reset_index()
 
-def postfilter_items():
+    #    department_size.columns = ['department', 'n_items']
+    #    rare_departments = department_size[department_size['n_items'] < 150].department.tolist()
+    #    items_in_rare_departments = item_features[item_features['department'].isin(rare_departments)].item_id.unique().tolist()
+
+    #    data = data[~data['item_id'].isin(items_in_rare_departments)]
+
+
+    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб.
+    data['price'] = data['sales_value'] / (np.maximum(data['quantity'], 1))
+    data = data[data['price'] > 0.50]
+
+    # Уберем слишком дорогие товарыs
+    data = data[data['price'] < 30]
+
+    # Возбмем топ по популярности
+    popularity = data.groupby('item_id')['quantity'].sum().reset_index()
+    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
+
+    top = popularity.sort_values('n_sold', ascending=False).head(take_n_popular).item_id.tolist()
+    
+    # Заведем фиктивный item_id (если юзер покупал товары из топ-5000, то он "купил" такой товар)
+    data.loc[~data['item_id'].isin(top), 'item_id'] = 999999
+    
+    # ...
+
+    return data
+
+
+def postfilter_items(user_id, recommednations):
     pass
-
-def get_recommendations(user, model, sparse_user_item, N=5):
-    """Рекомендуем топ-N товаров"""
-    
-    res = [id_to_itemid[rec[0]] for rec in 
-                    model.recommend(userid=userid_to_id[user], 
-                                    user_items=sparse_user_item,   # на вход user-item matrix
-                                    N=N, 
-                                    filter_already_liked_items=False, 
-                                    #filter_items=[itemid_to_id[999999]],  # !!! 
-                                    recalculate_user=True)]
-    return res
